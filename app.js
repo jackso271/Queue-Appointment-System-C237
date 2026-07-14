88b4ef397716b757ca1ab2242fd3d131ef0a94d1
jackso271 <25044459@myrp.edu.sg>
2026-07-14 12:27:34 +0800

    Adding database connection

---
+++ app.js
@@ -0,0 +1,12 @@
+// Database connection
+const db = mysql.createConnection({
+    host: 'c237-eaint-mysql.mysql.database.azure.com',
+    user: 'c237_001',
+    password: 'c237001@2026!',
+    database: 'c237_001_teamaplus',
+    //It tells your app to talk to the Azure database using SSL, which is required by Azure for secure connections.
+    //Which Azure requires to use SSL for secure connections to the database. The rejectUnauthorized: true option ensures that the SSL certificate is verified.
+    ssl: {
+        rejectUnauthorized: true
+    }
+});
